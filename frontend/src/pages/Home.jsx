f357bc79228f9e79e5b9886a5019ecbfc22ed0ad
imaaryan <imaaryanpandey@gmail.com>
2025-09-10 16:50:16 +0530

    Contact Form Added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../sections/Home/HeroSection";
 import { BlogsList } from "../sections/Home/BlogsTab";
+import ContactForm from "../sections/Home/ContactForm";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <BlogsList />
+      <ContactForm />
     </>
   );
 };
