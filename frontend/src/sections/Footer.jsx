9a37fa9756ed800814edab90338843a00a738fb2
imaaryan <imaaryanpandey@gmail.com>
2025-09-10 16:39:22 +0530

    Logo Update

--- frontend/src/sections/Footer.jsx
+++ frontend/src/sections/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {assets} from "../assets/assets.js"
 
 const Footer = () => {
   return (
@@ -7,16 +8,16 @@ const Footer = () => {
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" className="flex items-center">
+              <div href="https://flowbite.com/" className="flex items-center">
                 <img
-                  src="https://flowbite.com/docs/images/logo.svg"
+                  src={assets.logo}
                   className="h-8 me-3"
                   alt="FlowBite Logo"
                 />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  Flowbite
+                  ReadOutLoud
                 </span>
-              </a>
+              </div>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
@@ -84,9 +85,9 @@ const Footer = () => {
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-              © 2023{" "}
+              © 2025{" "}
               <a href="https://flowbite.com/" className="hover:underline">
-                Flowbite™
+                ReadOutLoud
               </a>
               . All Rights Reserved.
             </span>
