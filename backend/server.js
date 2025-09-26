68482ca9a4d0b32a464de57de3cc935aa006ebf5
imaaryan <imaaryanpandey@gmail.com>
2025-09-26 15:00:25 +0530

    Basic Server Boilerplate Setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors"
+import dotenv from "dotenv"
+
+const app = express()
+dotenv.config()
+
+//Middlewares
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.get('/', (req, res) => {
+  res.send('Server is Running')
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log("Server is Running on PORT " + PORT)
+})
+
+export default app;
